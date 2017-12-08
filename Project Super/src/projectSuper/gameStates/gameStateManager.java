18a8da27559593d637e3dafa910d130fd68e2f1d
Kion-Smith <kion_smith@hotmail.com>
2017-12-07 23:55:16 -0600

    adding the game state manager

--- Project Super/src/projectSuper/gameStates/gameStateManager.java
+++ Project Super/src/projectSuper/gameStates/gameStateManager.java
@@ -1,9 +1,71 @@
 package projectSuper.gameStates;
 
+import java.awt.Graphics2D;
+
+
 public class gameStateManager 
 {
-	public gameStateManager()
-	{
+	
+		private boolean paused;
 		
-	}
+		private gameState[] gS;
+		private int curState;
+		private int prevState;
+		
+		public static final int numStates = 4;
+		public static final int MENU = 0;
+		public static final int PLAY = 1;
+		public static final int HELP = 2;
+		public static final int GAMEOVER = 3;
+		
+		public gameStateManager()
+		{
+			paused = false;
+			
+			gS = new gameState[numStates];
+			
+			setState(MENU);
+		}
+		
+		public void setState(int i)
+		{
+			prevState = curState;
+			unloadState(prevState);
+			curState = i;
+			
+			//add states
+		}
+		
+		public void unloadState(int i)
+		{
+			gS[i] = null;
+		}
+		
+		public void setPaused(boolean b)
+		{
+			paused = b;
+		}
+		
+		public void update()
+		{
+		
+			if(!paused)
+			{
+				gS[curState].update();
+			}
+
+		}
+		
+		public void draw(Graphics2D g)
+		{
+
+			if(!paused)
+			{
+				gS[curState].draw(g);
+			}
+
+		}
+		
+	
+
 }
