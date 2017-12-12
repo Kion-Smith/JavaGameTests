95a2dfbfdd77c4f2dad86ea102e7f5176be7aa72
Kion-Smith <kion_smith@hotmail.com>
2017-12-11 23:44:36 -0600

    prints random tiles

--- Project Super/src/Graphics/Screen.java
+++ Project Super/src/Graphics/Screen.java
@@ -1,9 +1,15 @@
 package Graphics;
+
+import java.util.Random;
+
 //pixel manipulation class
 public class Screen 
 {
 	private int width,height; //size of screen
 	public int[] pixels; //array for pixels on screen, single array because its faster than using a 2d array
+	
+	public int[] tiles = new int[64*64];
+	private Random random = new Random();
 	
 	
 	public Screen(int w,int h)
@@ -12,6 +18,11 @@ public class Screen
 		height =h;
 		
 		pixels = new int[width * height]; // all pixels on the screen
+		
+		for(int i =0;i<64*64;i++)
+		{
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 	
 	public void render()
@@ -21,7 +32,8 @@ public class Screen
 		{
 			for(int j = 0;j<width;j++) //looping through the row at the col
 			{
-				pixels[j + i* width] =  0xffff00ff;// because this is single dimension array so looks for position needs to offset by the width
+				int tileIndex = (j/32)+(i/32) *64;
+				pixels[j + i* width] =  tiles[tileIndex];// because this is single dimension array so looks for position needs to offset by the width
 			}
 		}
 
