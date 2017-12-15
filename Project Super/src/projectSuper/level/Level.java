534d6e292f1bbd3690d8c9590605686f9d850e18
Kion-Smith <kion_smith@hotmail.com>
2017-12-15 14:15:33 -0600

    method for creating levels

---
+++ Project Super/src/projectSuper/level/Level.java
@@ -0,0 +1,45 @@
+package projectSuper.level;
+
+import projectSuper.graphics.Screen;
+
+public class Level 
+{
+	private int width,height;
+	private int[] tiles;
+	
+	public Level(int w,int h)
+	{
+		width = w;
+		height = h;
+		tiles = new int[width*height];
+		generateLevel();
+	}
+	
+	public Level(String path)
+	{
+		loadLevel(path);
+	}
+	
+	public void generateLevel()
+	{
+		
+	}
+	
+	private void loadLevel(String path)
+	{
+		
+	}
+	
+	public void update()
+	{
+		
+	}
+	private void time()
+	{
+		
+	}
+	public void render(int xScroll,int yScroll,Screen screen)
+	{
+		
+	}
+}
