36a924f2ea2ebf95e2885b4fb89d079b6dd218f1
Kion-Smith <kion_smith@hotmail.com>
2017-12-16 21:52:37 -0600

    creating ground tile

--- Project Super/src/projectSuper/level/tiles/Tile.java
+++ Project Super/src/projectSuper/level/tiles/Tile.java
@@ -8,6 +8,9 @@ public class Tile
 	public int x, y;
 	public Sprite sprite;
 	
+	public static Tile ground = new ground1Tile(Sprite.ground1);//only one object
+	
+	
 	public Tile(Sprite s) 
 	{
 		sprite = s;
---
+++ Project Super/src/projectSuper/level/tiles/ground1Tile.java
@@ -0,0 +1,19 @@
+package projectSuper.level.tiles;
+
+import projectSuper.graphics.Screen;
+import projectSuper.graphics.Sprite;
+
+public class ground1Tile extends Tile
+{
+
+	public ground1Tile(Sprite s) 
+	{
+		super(s);
+	}
+	
+	public void render(int locX,int locY,Screen s)
+	{
+		
+	}
+
+}
